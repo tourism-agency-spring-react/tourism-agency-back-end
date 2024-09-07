01be0c1ac2391b7ed654acf3c77e977f5ef71a1f
Israel <israel.bastion@gmail.com>
2024-09-06 20:32:15 -0500

    first commit

---
+++ src/main/java/com/tourismagency/tourism_agency/TourismAgencyApplication.java
@@ -0,0 +1,13 @@
+package com.tourismagency.tourism_agency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourismAgencyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TourismAgencyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tourismagency/tourism_agency/TourismAgencyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tourismagency.tourism_agency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TourismAgencyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
