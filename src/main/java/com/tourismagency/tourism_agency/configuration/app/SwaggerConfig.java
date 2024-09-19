a47bf5da144ab61afc72e27f04fcb28c68b98244
Israel <israel.bastion@gmail.com>
2024-09-19 17:22:53 -0500

    Cors basic added.

--- src/main/java/com/tourismagency/tourism_agency/configuration/app/SwaggerConfig.java
+++ src/main/java/com/tourismagency/tourism_agency/configuration/app/SwaggerConfig.java
@@ -1,15 +1,10 @@
 package com.tourismagency.tourism_agency.configuration.app;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.info.License;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
-import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.annotations.servers.Server;
-import org.springframework.http.HttpHeaders;
 
 @OpenAPIDefinition(
         info = @Info(
@@ -36,20 +31,7 @@ import org.springframework.http.HttpHeaders;
                         description = "prod server",
                         url = "https://tourism-agency-api:8080"
                 ),
-        },
-        security = @SecurityRequirement(
-                name = "Security Token"
-        )
-)
-
-@SecurityScheme(
-        name = "Security Token",
-        description = "Access Token for my API",
-        type = SecuritySchemeType.HTTP,
-        paramName = HttpHeaders.AUTHORIZATION,
-        in = SecuritySchemeIn.HEADER,
-        scheme = "bearer",
-        bearerFormat = "JWT"
+        }
 )
 public class SwaggerConfig {
 
