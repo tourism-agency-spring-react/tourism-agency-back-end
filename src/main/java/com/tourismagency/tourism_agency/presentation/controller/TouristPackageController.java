dae94752892206f1be53492b73d95a3d99834dc1
Marjhorick <venturamarjhorick@gmail.com>
2024-10-06 21:45:00 -0500

    Documentacion de TouristPackage y TouristService

--- src/main/java/com/tourismagency/tourism_agency/presentation/controller/TouristPackageController.java
+++ src/main/java/com/tourismagency/tourism_agency/presentation/controller/TouristPackageController.java
@@ -3,6 +3,11 @@ package com.tourismagency.tourism_agency.presentation.controller;
 import com.tourismagency.tourism_agency.presentation.dto.TouristPackageDTO;
 import com.tourismagency.tourism_agency.service.exception.ResourceNotFoundException;
 import com.tourismagency.tourism_agency.service.interfaces.ITouristPackage;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -15,11 +20,34 @@ import java.util.List;
 @RequiredArgsConstructor
 @RequestMapping("/api/v1")
 @RestController
+@Tag(name = "TouristPackage Controller", description = "Controller for TouristPackage crud")
 public class TouristPackageController {
 
     private final ITouristPackage touristPackageService;
 
     @GetMapping("/packages")
+    @Operation (
+            summary = "Get all TouristPackage",
+            description = "Return all touristPackage from the data base in a format json list.",
+            tags = { "TouristPackage Controller" },
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    description = "Has no data",
+                    required = true,
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = TouristPackageDTO.class)
+                    )
+            ),
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = TouristPackageDTO.class)
+                            )
+                    )
+            }
+    )
     public ResponseEntity<?> getAllTouristPackages() {
         try {
             List<TouristPackageDTO> touristPackageDTOList = touristPackageService.getAll();
@@ -30,6 +58,28 @@ public class TouristPackageController {
     }
 
     @GetMapping("/package/{id}")
+    @Operation (
+            summary = "Get a TouristPackage",
+            description = "Return a touristPackage by ID",
+            tags = { "TouristPackage Controller" },
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    description = "Has no data",
+                    required = true,
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = TouristPackageDTO.class)
+                    )
+            ),
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = TouristPackageDTO.class)
+                            )
+                    )
+            }
+    )
     public ResponseEntity<?> getTouristPackage(@PathVariable Long id) {
         try {
             TouristPackageDTO touristPackageDTO = touristPackageService.getById(id);
@@ -40,6 +90,28 @@ public class TouristPackageController {
     }
 
     @PostMapping("/package")
+    @Operation (
+            summary = "Create a TouristPackage",
+            description = "Receives data from touristPackage in json format to stores it in the data base and send confirm message in the body.",
+            tags = { "TouristPackage Controller" },
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    description = "Data from touristPackage in format json",
+                    required = true,
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = TouristPackageDTO.class)
+                    )
+            ),
+            responses = {
+                    @ApiResponse(
+                            responseCode = "201",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = TouristPackageDTO.class)
+                            )
+                    )
+            }
+    )
     public ResponseEntity<?> createTouristPackage (@RequestBody @Valid TouristPackageDTO touristPackageDTO) {
         try {
             touristPackageService.save(touristPackageDTO);
@@ -50,6 +122,28 @@ public class TouristPackageController {
     }
 
     @PutMapping("/package")
+    @Operation (
+            summary = "Update a TouristPackage",
+            description = "Find a touristPackage by ID to updates her data base and send confirm message in the body.",
+            tags = { "TouristPackage Controller" },
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    description = "New data from touristPackage in format json for the update process.",
+                    required = true,
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = TouristPackageDTO.class)
+                    )
+            ),
+            responses = {
+                    @ApiResponse(
+                            responseCode = "201",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = TouristPackageDTO.class)
+                            )
+                    )
+            }
+    )
     public ResponseEntity<?> updateTouristPackage(@RequestBody @Valid TouristPackageDTO touristPackageDTO) {
         try{
             touristPackageService.update(touristPackageDTO.id(), touristPackageDTO);
@@ -60,6 +154,28 @@ public class TouristPackageController {
     }
 
     @DeleteMapping("/package/{id}")
+    @Operation (
+            summary = "Delete a TouristPackage",
+            description = "Find the touristPackage by ID to deletes it and send confirm message in the body.",
+            tags = { "TouristPackage Controller" },
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    description = "Has no data",
+                    required = true,
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = TouristPackageDTO.class)
+                    )
+            ),
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = TouristPackageDTO.class)
+                            )
+                    )
+            }
+    )
     public ResponseEntity<?> deleteTouristPackage(@PathVariable Long id) {
         try {
             touristPackageService.delete(id);
--- src/main/java/com/tourismagency/tourism_agency/presentation/controller/TouristServiceController.java
+++ src/main/java/com/tourismagency/tourism_agency/presentation/controller/TouristServiceController.java
@@ -3,6 +3,11 @@ package com.tourismagency.tourism_agency.presentation.controller;
 import com.tourismagency.tourism_agency.presentation.dto.TouristServiceDTO;
 import com.tourismagency.tourism_agency.service.exception.ResourceNotFoundException;
 import com.tourismagency.tourism_agency.service.interfaces.ITouristService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -15,11 +20,34 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1")
+@Tag(name = "TouristService Controller", description = "Controller for TouristService crud")
 public class TouristServiceController {
 
     private final ITouristService touristServiceService;
 
     @GetMapping("/services")
+    @Operation(
+            summary = "Get all TouristService",
+            description = " Return all touristService from the data base in a format json list.",
+            tags = { "TouristService Controller" },
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    description = "Has no data",
+                    required = true,
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = TouristServiceDTO.class)
+                    )
+            ),
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = TouristServiceDTO.class)
+                            )
+                    )
+            }
+    )
     public ResponseEntity<?> getAllTouristService() {
         try {
             List<TouristServiceDTO> touristServiceDTOList = touristServiceService.getAll();
@@ -30,6 +58,28 @@ public class TouristServiceController {
     }
 
     @GetMapping("/service/{id}")
+    @Operation (
+            summary = "Get a TouristService",
+            description = "Return touristService by ID",
+            tags = { "TouristService Controller" },
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    description = "Has no data",
+                    required = true,
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = TouristServiceDTO.class)
+                    )
+            ),
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = TouristServiceDTO.class)
+                            )
+                    )
+            }
+    )
     public ResponseEntity<?> getTouristService(@PathVariable Long id) {
         try {
             TouristServiceDTO touristServiceDTO = touristServiceService.getById(id);
@@ -40,6 +90,28 @@ public class TouristServiceController {
     }
 
     @PostMapping("/service")
+    @Operation (
+            summary = "Create a TouristService",
+            description = "Receives data touristService in json format to stores it in the data base and send confirm message in the body.",
+            tags = { "TouristService Controller" },
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    description = "Has no data",
+                    required = true,
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = TouristServiceDTO.class)
+                    )
+            ),
+            responses = {
+                    @ApiResponse(
+                            responseCode = "201",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = TouristServiceDTO.class)
+                            )
+                    )
+            }
+    )
     public ResponseEntity<?> createTouristService(@RequestBody @Valid TouristServiceDTO touristServiceDTO) {
         try {
             touristServiceService.save(touristServiceDTO);
@@ -50,6 +122,28 @@ public class TouristServiceController {
     }
 
     @PutMapping("/service")
+    @Operation (
+            summary = "Update a TouristService",
+            description = "Find the touristService by ID to update her data base and send confirm message in the body.",
+            tags = { "TouristService Controller" },
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    description = "Has no data",
+                    required = true,
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = TouristServiceDTO.class)
+                    )
+            ),
+            responses = {
+                    @ApiResponse(
+                            responseCode = "201",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = TouristServiceDTO.class)
+                            )
+                    )
+            }
+    )
     public ResponseEntity<?> updateTouristService(@RequestBody @Valid TouristServiceDTO touristServiceDTO)  {
         try{
             touristServiceService.update(touristServiceDTO.id(), touristServiceDTO);
@@ -60,6 +154,29 @@ public class TouristServiceController {
     }
 
     @DeleteMapping("/service/{id}")
+    @Operation (
+            summary = "Delete a TouristService",
+            description = "Find the touristService by ID to deletes it and send confirm message in the body.",
+            tags = { "TouristService Controller" },
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    description = "Has no data",
+                    required = true,
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = TouristServiceDTO.class)
+                    )
+            ),
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = TouristServiceDTO
+                                            .class)
+                            )
+                    )
+            }
+    )
     public ResponseEntity<?> deleteTouristService(@PathVariable Long id) {
         try {
             touristServiceService.delete(id);
