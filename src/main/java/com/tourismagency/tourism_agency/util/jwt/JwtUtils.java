20ff3214cc5370e35eb71d12c17f14aaa7d95b64
MLxpezz <mauricio_lopez_098@hotmail.com>
2024-10-22 10:38:11 -0600

    add jwtUtils

--- src/main/java/com/tourismagency/tourism_agency/util/jwt/JwtUtils.java
+++ src/main/java/com/tourismagency/tourism_agency/util/jwt/JwtUtils.java
@@ -1,6 +1,7 @@
 package com.tourismagency.tourism_agency.util.jwt;
 
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 
 import org.springframework.security.core.Authentication;
@@ -30,7 +31,31 @@ public class JwtUtils {
                 .compact();
     }
 
+    public boolean isValidToken(String token) {
+        try {
+
+            Jwts.parser()
+                    .verifyWith(key)
+                    .build()
+                    .parseSignedClaims(token)
+                    .getPayload();
+
+            return true;
+        } catch (JwtException exception) {
+            System.out.println("El token es invalido: " + exception.getMessage());
+            return false;
+        }
+    }
+
     public Claims getClaims(String token) {
-        return null;
+        return Jwts.parser()
+                .verifyWith(key)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+    public String getEmailFromToken(String token) {
+        return getClaims(token).getSubject();
     }
 }
