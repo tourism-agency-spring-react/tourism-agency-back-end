53d51939a679057d646b11ee8571d95117eccbf6
Israel <israel.bastion@gmail.com>
2024-10-24 08:04:23 -0500

    xd

--- src/main/java/com/tourismagency/tourism_agency/presentation/controller/CustomerController.java
+++ src/main/java/com/tourismagency/tourism_agency/presentation/controller/CustomerController.java
@@ -46,10 +46,13 @@ public class CustomerController implements CustomerResource {
     @PostMapping("/customer")
     public ResponseEntity<MessageResponse> create (@RequestBody @Valid CustomerDTO customerDTO){
         LOGGER.info("Creating customer");
+
         customerService.save(customerDTO);
+
         MessageResponse response = MessageResponse.builder()
                 .message("Registrado correctamente.")
                 .build();
+
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
